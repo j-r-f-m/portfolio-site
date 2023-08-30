84280793e78e0bbe862ffdcb59eb1007a5f29e6f
jrfm <moesch.jonas@protonmail.com>
2023-08-30 15:57:30 +0200

    create footer component

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import Container from "react-bootstrap/Container";
+
+export function Footer() {
+  return (
+    <>
+      <Container
+        className="border border-danger bg-dark text-white-50 mt-5  d-flex  justify-content-center  align-items-center"
+        fluid
+      >
+        Copyright
+      </Container>
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Navigation } from "../components/navigation/Navigation";
 import { MainContent } from "../pages/mainContent/MainContent";
+import { Footer } from "../components/footer/Footer";
 
 function App() {
   return (
     <>
       <Navigation />
       <MainContent />
+      <Footer />
     </>
   );
 }
