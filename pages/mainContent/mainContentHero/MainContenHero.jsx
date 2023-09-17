198c1856e77f2b545e86bb7d4f075a039387418b
jrfm <moesch.jonas@protonmail.com>
2023-09-17 13:44:52 +0200

    Add content to MainContentHero.jsx
    Add html elemets for thechnologies used as an engnineer and as a
    programmer

--- pages/mainContent/mainContentHero/MainContenHero.jsx
+++ pages/mainContent/mainContentHero/MainContenHero.jsx
@@ -16,8 +16,25 @@ export function MainContentHero() {
           <h2>About Me</h2>
         </Row>
         <Row className="d-flex">
-          <Col sm className=" d-flex justify-content-center align-items-center">
+          <Col
+            sm
+            className=" d-flex justify-content-center align-items-center mb-2"
+          >
             <article> {aboutMeEng}</article>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mb-2">
+            <article>Technologies I use as a programmer:</article>
+            <article>
+              Javascript, Python, React.js, git, bootstrap, vite
+            </article>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <article>Technologies I use as a structural engineer:</article>
+            <article>Rfem, Frilo, Harzer, Revit, Briscad</article>
           </Col>
         </Row>
         <hr />
