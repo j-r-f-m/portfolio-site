2d0be9cff3c5a9ae4c2a0c6e9e24445f5bbea9e2
jrfm <moesch.jonas@protonmail.com>
2023-10-23 20:56:41 +0200

    add working links to one project

--- pages/mainContent/mainContentProjects/MainContentProjectsLinks.jsx
+++ pages/mainContent/mainContentProjects/MainContentProjectsLinks.jsx
@@ -1,17 +1,32 @@
 import Nav from "react-bootstrap/Nav";
 
+// export function LinkComponent()
+
+/**
+ * Renders the links for a project in the main content section.
+ * @returns {JSX.Element} The JSX code for the project links.
+ */
+
 export function MainContentProjectsLinks() {
   return (
     <>
       <Nav variant="underline">
-        <Nav.Item className="d-flex align-items-center">
-          <Nav.Link className=" text-light" href="" style={{ padding: "1px" }}>
+        <Nav.Item className="">
+          <Nav.Link
+            className=" text-light"
+            href="https://hilarious-tiramisu-a7e4c7.netlify.app"
+            style={{ padding: "1px" }}
+          >
             <span>Visit Site</span>
           </Nav.Link>
         </Nav.Item>
 
         <Nav.Item>
-          <Nav.Link className="text-light" href="" style={{ padding: "1px" }}>
+          <Nav.Link
+            className="text-light"
+            href="https://github.com/j-r-f-m/portfolio-site"
+            style={{ padding: "1px" }}
+          >
             View Code
           </Nav.Link>
         </Nav.Item>
--- pages/mainContent/mainContentProjects/MainContentProjectsLinks.test.jsx
+++ pages/mainContent/mainContentProjects/MainContentProjectsLinks.test.jsx
@@ -1,18 +1,36 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-
+import "@testing-library/jest-dom";
 import { MainContentProjectsLinks } from "./MainContentProjectsLinks";
 
 describe("MainContentProjectsLinks component", () => {
-  it("Renders Projects Links", () => {
+  it("renders Visit Site and View Code links", () => {
     render(<MainContentProjectsLinks />);
-    screen.debug();
+    expect(
+      screen.getByRole("link", { name: "Visit Site" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "View Code" })).toBeInTheDocument();
+  });
 
-    // expect(screen.getByRole("heading", { level: 1 }).textContent).toMatch(
-    //   /Projects/i
-    // );
+  it("has the correct href for Visit Site and View Code links", () => {
+    render(<MainContentProjectsLinks />);
+    expect(screen.getByRole("link", { name: "Visit Site" })).toHaveAttribute(
+      "href",
+      "https://hilarious-tiramisu-a7e4c7.netlify.app"
+    );
+    expect(screen.getByRole("link", { name: "View Code" })).toHaveAttribute(
+      "href",
+      "https://github.com/j-r-f-m/portfolio-site"
+    );
+  });
 
-    expect(screen.getByRole("button", { name: "Visit Site" }));
-    expect(screen.getByRole("button", { name: "View Code" }));
+  it("has the correct styling for Visit Site and View Code links", () => {
+    render(<MainContentProjectsLinks />);
+    expect(screen.getByRole("link", { name: "Visit Site" })).toHaveStyle({
+      padding: "1px",
+    });
+    expect(screen.getByRole("link", { name: "View Code" })).toHaveStyle({
+      padding: "1px",
+    });
   });
 });
